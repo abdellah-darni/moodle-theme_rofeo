7766640255a270a4b2c52fdb7e834f2774cbb81f
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-08-10 11:41:49 +0100

    feat: add French language strings for ROFEO theme

---
+++ lang/fr/theme_rofeo.php
@@ -0,0 +1,26 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'ROFEO';
+$string['configtitle'] = 'ROFEO';
+$string['choosereadme'] = 'Thème ROFEO Academy, basé sur Moove.';
+
+$string['heroheading'] = 'Bannière d\'accueil';
+$string['herotitle'] = 'Titre de la bannière';
+$string['herotitle_desc'] = 'Le titre principal en haut de la page d\'accueil.';
+$string['herosubtitle'] = 'Sous-titre de la bannière';
+$string['herosubtitle_desc'] = 'Une ou deux phrases sous le titre.';
+$string['heroctalabel'] = 'Libellé du bouton';
+$string['heroctalabel_desc'] = 'Texte affiché sur le bouton principal.';
+$string['heroctaurl'] = 'Lien du bouton';
+$string['heroctaurl_desc'] = 'Destination du bouton. Utilisez #catalogue pour faire défiler jusqu\'au catalogue.';
+$string['heroimage'] = 'Image de la bannière';
+$string['heroimage_desc'] = 'Image affichée en arrière-plan de la bannière. Format paysage, au moins 1200px de large. Laissez vide pour utiliser l\'image par défaut du thème.';
+
+$string['catalogueheading'] = 'Catalogue des formations';
+$string['cataloguetitle'] = 'Titre du catalogue';
+$string['cataloguetitle_desc'] = 'Titre affiché au-dessus du catalogue sur la page d\'accueil.';
+$string['coursectalabel'] = 'Libellé du bouton des formations';
+$string['coursectalabel_desc'] = 'Texte du bouton sous chaque carte de formation.';
+
+$string['footerwebsite'] = 'Site web';
